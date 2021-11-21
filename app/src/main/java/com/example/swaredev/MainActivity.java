18e1b5f9ee9d338c04e340c8def13d9f751e533a
rono516 <ronocollins2000@gmail.com>
2021-11-21 11:30:00 +0300

    android

---
+++ app/src/main/java/com/example/swaredev/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.swaredev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button buttonfirst = (Button)findViewById(R.id.buttonfirst);
+        buttonfirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSecondActivity();
+            }
+        });
+    }
+
+    private void goToSecondActivity() {
+        Intent n = new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(n);
+
+    }
+}
